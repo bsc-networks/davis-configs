3e43d611d9386a284d5f6fe12c2b3674c083e6a3
Nicholas Herson <nicholas.herson@gmail.com>
2014-03-26 00:52:54 -0700

    adding base chef-repo and playing around with a test cookbook

---
+++ chef-repo/cookbooks/test_cookbook/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: test_cookbook
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "zsh" do
+  action :install
+end
