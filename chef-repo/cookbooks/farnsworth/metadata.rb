fc4b6b038d370ff5ca31c5f8cc0bf31a83944a4d
Nicholas Herson <nicholas.herson@gmail.com>
2014-07-01 22:41:30 -0700

    adding basic cookbook files for farnsworth

---
+++ chef-repo/cookbooks/farnsworth/metadata.rb
@@ -0,0 +1,7 @@
+name             'farnsworth'
+maintainer       'Nicholas Herson'
+maintainer_email 'nicholas.herson@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures farnsworth'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ chef-repo/cookbooks/farnsworth/recipes/default.rb
@@ -0,0 +1,9 @@
+# Cookbook Name:: farnsworth
+# Recipe:: default
+#
+# Copyright 2014, Nicholas Herson, Berkeley Student Cooperative
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
