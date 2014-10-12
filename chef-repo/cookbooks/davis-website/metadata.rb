8cca00d20d1ca66a3e98858103c259098e18f683
Nicholas Herson <nicholas.herson@gmail.com>
2014-10-11 23:12:34 -0700

    change password and user for NYT info, pw changed in data bag

--- chef-repo/cookbooks/davis-website/metadata.rb
+++ chef-repo/cookbooks/davis-website/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures davis-website'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.1.0'
+version          '1.2.0'
 
 depends 'apache2'
