5e693e23190ed08b4ae359fb97d718a3b71f28ad
Nicholas Herson <nicholas.herson@gmail.com>
2015-02-14 16:04:38 -0800

    initial reworked davis-unifi cookbook

---
+++ chef-repo/cookbooks/davis-unifi/metadata.rb
@@ -0,0 +1,9 @@
+name             'davis-unifi'
+maintainer       'Davis Network Manager'
+maintainer_email 'davnm@bsc.coop'
+license          'All rights reserved'
+description      'Installs/Configures davis-unifi'
+long_description 'Installs/Configures davis-unifi'
+version          '0.1.0'
+
+depends 'unifi'
